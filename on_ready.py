8adfbbf2018fabf27d3fcfce4b3f5b23713577a9
Guenther Schmitz <727316+gpunktschmitz@users.noreply.github.com>
2021-05-12 16:57:25 +0200

    set mic to be used explicitly

--- on_ready.py
+++ on_ready.py
@@ -1,4 +1,5 @@
 from talon import actions, app
 def on_ready():
     actions.speech.disable()
+    actions.speech.set_microphone('Yeti Stereo Microphone Analog Stereo')
 app.register('launch', on_ready)
