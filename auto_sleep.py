530223a3971315a09a7527d4c3af564d627bc967
Guenther Schmitz <727316+gpunktschmitz@users.noreply.github.com>
2022-12-17 09:27:22 +0100

    fixed intent

--- auto_sleep.py
+++ auto_sleep.py
@@ -9,7 +9,7 @@ speech_system.register('post:phrase', on_phrase)
 def on_ready():
     global job
     if actions.speech.enabled():
-       job = cron.after('2m', actions.speech.disable)
+        job = cron.after('2m', actions.speech.disable)
 app.register("ready", on_ready)
 # https://github.com/nriley/knausj_talon/blob/nriley/misc/screen.py
 ui.register('screen_sleep', lambda e: actions.speech.disable())
