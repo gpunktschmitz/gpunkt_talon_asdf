262e2586a55ae72b8413cebbcd86161191cc9218
Guenther Schmitz <727316+gpunktschmitz@users.noreply.github.com>
2022-12-17 08:57:34 +0100

    enable eye tracking on startup

--- on_ready.py
+++ on_ready.py
@@ -2,4 +2,5 @@ from talon import actions, app
 def on_ready():
     actions.speech.disable()
     actions.speech.set_microphone('Yeti Stereo Microphone Analog Stereo')
+    actions.tracking.control_toggle()
 app.register('launch', on_ready)
