39fd99148dc2b5342de0e23e54a58e30747ba201
Guenther Schmitz <727316+gpunktschmitz@users.noreply.github.com>
2022-12-17 08:58:33 +0100

    removed speech disable on startup

--- on_ready.py
+++ on_ready.py
@@ -1,6 +1,5 @@
 from talon import actions, app
 def on_ready():
-    actions.speech.disable()
     actions.speech.set_microphone('Yeti Stereo Microphone Analog Stereo')
     actions.tracking.control_toggle()
 app.register('launch', on_ready)
