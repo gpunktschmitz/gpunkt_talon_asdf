cadbeac6cac90aff729a3cf4a93c67ee9a470f86
Guenther Schmitz <727316+gpunktschmitz@users.noreply.github.com>
2022-12-29 08:58:53 +0100

    updated auto_sleep to disable both, speech and mouse_control

--- auto_sleep.py
+++ auto_sleep.py
@@ -1,16 +1,28 @@
 from talon import ui, actions, cron, speech_system, app
+
 job = None
+timeout = '5m'
+
+def handle_timeout():
+    actions.speech.disable()
+    if actions.tracking.control_enabled():
+        actions.tracking.control_toggle()
+
 def on_phrase(j):
     global job
     if j.get('phrase'):
         cron.cancel(job)
-        job = cron.after('2m', actions.speech.disable)
+        job = cron.after(timeout, handle_timeout)
+
 speech_system.register('post:phrase', on_phrase)
+
 def on_ready():
     global job
     if actions.speech.enabled():
-        job = cron.after('2m', actions.speech.disable)
+        job = cron.after(timeout, handle_timeout)
+
 app.register("ready", on_ready)
+
 # https://github.com/nriley/knausj_talon/blob/nriley/misc/screen.py
 ui.register('screen_sleep', lambda e: actions.speech.disable())
 # ui.register('screen_wake')
